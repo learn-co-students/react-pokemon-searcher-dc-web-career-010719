f10ec21ade12dfe3d884582f657a48ddf1a95868
James Clement <jameseclement@gmail.com>
2019-03-17 21:39:36 -0400

    done

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,27 +1,47 @@
-import React from 'react'
-import { Card } from 'semantic-ui-react'
+import React from "react";
+import { Card } from "semantic-ui-react";
 
 class PokemonCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      front: true
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      front: !this.state.front
+    });
+  };
+
   render() {
     return (
-      <Card>
+      <Card onClick={this.handleClick}>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img
+              src={
+                this.state.front
+                  ? this.props.pokemonObj.sprites.front
+                  : this.props.pokemonObj.sprites.back
+              }
+              alt="oh no!"
+            />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.pokemonObj.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {this.props.pokemonObj.stats[5].value} HP
             </span>
           </div>
         </div>
       </Card>
-    )
+    );
   }
 }
 
-export default PokemonCard
+export default PokemonCard;
--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,34 +1,108 @@
-import React from 'react'
-import { Form } from 'semantic-ui-react'
+import React from "react";
+import { Form } from "semantic-ui-react";
 
 class PokemonForm extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
-      name: '',
-      hp: '',
-      frontUrl: '',
-      backUrl: ''
-    }
+      name: "",
+      hp: "",
+      frontUrl: "",
+      backUrl: ""
+    };
   }
 
+  handleNameChange = target => {
+    this.setState({
+      name: target.value
+    });
+  };
+  handleHpChange = target => {
+    this.setState({
+      hp: target.value
+    });
+  };
+  handleFrontUrlChange = target => {
+    this.setState({
+      frontUrl: target.value
+    });
+  };
+  handleBackUrlChange = target => {
+    this.setState({
+      backUrl: target.value
+    });
+  };
+
+  handleSubmitForm = () => {
+    fetch("http://localhost:3000/pokemon", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        name: this.state.name,
+        stats: [
+          {},
+          {},
+          {},
+          {},
+          {},
+          {
+            value: this.state.hp,
+            name: "hp"
+          }
+        ],
+        sprites: {
+          front: this.state.frontUrl,
+          back: this.state.backUrl
+        }
+      })
+    })
+      .then(resp => resp.json())
+      .then(pokemon => this.props.addPokemon(pokemon));
+  };
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
-        <Form onSubmit={this.handleSubmit}>
+        <Form>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input
+              onChange={e => this.handleNameChange(e.target)}
+              fluid
+              label="Name"
+              placeholder="Name"
+              name="name"
+            />
+            <Form.Input
+              onChange={e => this.handleHpChange(e.target)}
+              fluid
+              label="hp"
+              placeholder="hp"
+              name="hp"
+            />
+            <Form.Input
+              onChange={e => this.handleFrontUrlChange(e.target)}
+              fluid
+              label="Front Image URL"
+              placeholder="url"
+              name="frontUrl"
+            />
+            <Form.Input
+              onChange={e => this.handleBackUrlChange(e.target)}
+              fluid
+              label="Back Image URL"
+              placeholder="url"
+              name="backUrl"
+            />
           </Form.Group>
-          <Form.Button>Submit</Form.Button>
+          <Form.Button onClick={this.handleSubmitForm}>Submit</Form.Button>
         </Form>
       </div>
-    )
+    );
   }
 }
 
-export default PokemonForm
+export default PokemonForm;
